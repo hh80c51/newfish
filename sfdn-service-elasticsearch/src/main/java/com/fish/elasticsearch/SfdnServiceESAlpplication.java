2da8b6a449a2e797ed80daffec725acccb16534f
unknown <hh80c51>
2020-11-04 18:42:14 +0800

    elasticsearch客户端

--- sfdn-service-elasticsearch/src/main/java/com/fish/elasticsearch/SfdnServiceESAlpplication.java
+++ sfdn-service-elasticsearch/src/main/java/com/fish/elasticsearch/SfdnServiceESAlpplication.java
@@ -1,20 +1,5 @@
 package com.fish.elasticsearch;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentType;
-
-import java.io.IOException;
-
 
 /**
  * @author hh
@@ -23,7 +8,7 @@ import java.io.IOException;
  */
 public class SfdnServiceESAlpplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
     }
 
--- sfdn-service-elasticsearch/src/main/java/com/fish/elasticsearch/util/ElasticSearchClient.java
+++ sfdn-service-elasticsearch/src/main/java/com/fish/elasticsearch/util/ElasticSearchClient.java
@@ -1,5 +1,10 @@
 package com.fish.elasticsearch.util;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.indices.CreateIndex;
+import io.searchbox.indices.mapping.PutMapping;
 import org.apache.http.HttpHost;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.ActionListener;
@@ -7,6 +12,7 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 
@@ -23,79 +29,26 @@ import java.util.Map;
  * @date 2020/11/3  23:54
  */
 public class ElasticSearchClient {
-    private String[] hostsAndPorts;
-
-    public ElasticSearchClient(String[] hostsAndPorts) {
-        this.hostsAndPorts = hostsAndPorts;
-    }
-    public RestHighLevelClient getClient() {
-        RestHighLevelClient client = null;
-        List<HttpHost> httpHosts = new ArrayList<HttpHost>();
-        if (hostsAndPorts.length > 0) {
-            for (String hostsAndPort : hostsAndPorts) {
-                String[] hp = hostsAndPort.split(":");
-                httpHosts.add(new HttpHost(hp[0], Integer.valueOf(hp[1]), "http"));
-            }
-            client = new RestHighLevelClient(
-                    RestClient.builder(httpHosts.toArray(new HttpHost[0])));
-        } else {
-            client = new RestHighLevelClient(
-                    RestClient.builder(new HttpHost("127.0.0.1", 9200, "http")));
-        }
-        return client;
-    }
-
-    private IndexRequest getIndexRequest(String index, String indexType, String docId, Map<String, Object> dataMap) {
-        IndexRequest indexRequest = null;
-        if (null == index || null == indexType) {
-            throw new ElasticsearchException("index or indexType must not be null");
-        }
-        if (null == docId) {
-            indexRequest = new IndexRequest(index, indexType);
-        } else {
-            indexRequest = new IndexRequest(index, indexType, docId);
-        }
-        return indexRequest;
-    }
-
-    /**
-     * 同步执行索引
-     *
-     * @param index
-     * @param indexType
-     * @param docId
-     * @param dataMap
-     * @throws IOException
-     */
-    public IndexResponse execIndex(String index, String indexType, String docId, Map<String, Object> dataMap) throws IOException {
-        return getClient().index(getIndexRequest(index, indexType, docId, dataMap).source(dataMap));
-    }
-
-    /**
-     * 异步执行
-     *
-     * @param index
-     * @param indexType
-     * @param docId
-     * @param dataMap
-     * @param indexResponseActionListener
-     * @throws IOException
-     */
-    public void asyncExecIndex(String index, String indexType, String docId, Map<String, Object> dataMap, ActionListener<IndexResponse> indexResponseActionListener) throws IOException {
-        getClient().indexAsync(getIndexRequest(index, indexType, docId, dataMap).source(dataMap), indexResponseActionListener);
-    }
 
     public static void main(String[] args) throws IOException {
-        XContentBuilder builder = XContentFactory.jsonBuilder();
-        builder.startObject();
-        {
-            builder.field("user", "kimchy");
-            builder.timeField("postDate", new Date());
-            builder.field("message", "trying out Elasticsearch");
-        }
-        builder.endObject();
-        IndexRequest indexRequest = new IndexRequest("posts", "doc", "1")
-                .source(builder);
-
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(new HttpClientConfig
+                .Builder("http://localhost:9200")
+                .multiThreaded(true)
+                //Per default this implementation will create no more than 2 concurrent connections per given route
+                .defaultMaxTotalConnectionPerRoute(2)
+                // and no more 20 connections in total
+                .maxTotalConnection(20)
+                        .build());
+        //创建一个索引
+        JestClient client = factory.getObject();
+//        client.execute(new CreateIndex.Builder("articles").build());
+        //创建一个映射
+        PutMapping putMapping = new PutMapping.Builder(
+                "articles",
+                "my_type",
+                "{ \"my_type\" : { \"properties\" : { \"message\" : {\"type\" : \"string\", \"store\" : \"yes\"} } } }"
+        ).build();
+        client.execute(putMapping);
     }
 }
